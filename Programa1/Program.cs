2829670ba1db0d4499ccd4ea069c3f2108e167f7
MiguelSanchezRosero <miguelroserosanchez@gmail.com>
2021-08-06 21:44:48 -0500

    Commit Inicial

---
+++ Programa1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+namespace Programa_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.- Limpiar la cocina");
+            Console.WriteLine("2.- Lavar la ropa");
+            Console.WriteLine("3.- Pasear al perro");
+            Console.WriteLine("4.- Hacer la comida");
+            Console.WriteLine("5.- Dormir temprano");
+        }
+    }
+}
---
+++ Programa2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+namespace Programa_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int VALOR1 = 25;
+            int VALOR2 = 5;
+            int VALOR3 = 2;
+            int Resultado = 0;
+
+            Resultado = (VALOR1 + VALOR2) * VALOR3;
+            Console.WriteLine("");
+            Console.WriteLine("La suma de {0} mas {1}, multiplicado por {2}, es igual a {3}", VALOR1, VALOR2, VALOR3, Resultado);
+
+        }
+    }
+}
